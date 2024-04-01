5c9c00d0cae2922132b3e7493acda99014a440db
HarliHaan <157319625+HarliHaan@users.noreply.github.com>
2024-04-02 01:12:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+ public class Main {
+
+    public static void main(String[] args) {
+
+        Shape shape = new Shape("red", false);
+        System.out.println(shape);
+
+
+        Shape circle = new Circle("blue", true, 20);
+        System.out.println(circle);
+
+
+
+
+    }
+
+
+}
